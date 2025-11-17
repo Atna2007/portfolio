ccb1e3d06d894357b612b1a265b03a99dce20a2a
Ariel Navas <navasariel44@gmail.com>
2025-11-16 22:00:30 -0500

    Updates

---
+++ assets/config-buGxs9ze.js
@@ -0,0 +1,5 @@
+const s="Ariel Navas",o="Creativo",a=["Desarrollador Web","Automatizaciones","IA","Bots","Diseños","Make","Scripts","Flujos","Chatbots","Integraciones","Consultas","Webhooks"],t="https://arielnavas.com",i="arielnavas.com",n="@ArielNavas",e="Atna2007",c="https://github.com/ArielNavas/portfolio",r=`__  __  __
+\\ \\ \\ \\ \\∕
+ \\ \\∕\\ \\
+  \\∕  \\∕
+`,l={name:s,role:o,disciplines:a,url:t,bluesky:i,figma:n,github:e,repo:c,ascii:r};export{l as c};
---
+++ assets/spr-lesson-builder-dark-BIt2ejS6.js
@@ -0,0 +1,1 @@
+const s="/assets/spr-lesson-builder-dark-large-Ba9wdo0p.jpg",r="/assets/spr-lesson-builder-dark-placeholder-BYjrS8rr.jpg",e="/assets/spr-lesson-builder-dark-CMYgxrTZ.jpg";export{s as a,r as b,e as s};
